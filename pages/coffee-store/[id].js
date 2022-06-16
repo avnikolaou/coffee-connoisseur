055055b49bc23cc0c277df760947b6e010d02676
avnik <avnikolaou@hotmail.com>
2022-06-16 15:17:11 +0300

    styling: add coffee store page styling

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,14 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import cls from 'classnames';
+
+import styles from '../../styles/coffee-store.module.css';
 
 import coffeeStoresData from '../../data/coffee-stores.json';
 
+// Next.js will pre-render this page at build time using the props returned by getStaticProps
 export function getStaticProps({ params }) {
   console.log('Params: ', params);
   return {
@@ -14,27 +20,87 @@ export function getStaticProps({ params }) {
   };
 }
 
+/*
+If a page has Dynamic Routes and uses getStaticProps,
+it needs to define a list of paths to be statically generated.
+
+When you export a function called getStaticPaths (Static Site Generation)
+from a page that uses dynamic routes, Next.js will statically
+pre-render all the paths specified by getStaticPaths.
+ */
+
 export function getStaticPaths({ params }) {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+
   return {
-    paths: [
-      { params: { id: '0' } },
-      { params: { id: '1' } },
-      { params: { id: '2' } },
-    ],
-    fallback: false,
+    paths,
+    fallback: true,
   };
 }
 
 const CoffeeStore = (props) => {
   const router = useRouter();
-  console.log('Router: ', router);
-  console.log('Props: ', props);
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const handleUpvoteButton = () => {
+    console.log('handle upvote');
+  };
+
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
   return (
-    <div>
-      Coffee store{router.query.id}
-      <Link href="/">Back to home</Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backtoHomeLink}>
+            <Link href="/">Back to home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls('glass', styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" width={24} height={24} />
+            <p className={styles.text}>{address}</p>
+          </div>
+
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" width={24} height={24} />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width={24} height={24} />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
