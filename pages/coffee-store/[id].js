4e6035c4374998be718a5c621892f483ee9a04b7
Avraam Nikolaou <avnikolaou@outlook.com>
2022-06-16 13:24:42 +0300

    feat: getStaticProps && getStaticPaths for coffeeStore page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,40 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export function getStaticProps({ params }) {
+  console.log('Params: ', params);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export function getStaticPaths({ params }) {
+  return {
+    paths: [
+      { params: { id: '0' } },
+      { params: { id: '1' } },
+      { params: { id: '2' } },
+    ],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
   console.log('Router: ', router);
+  console.log('Props: ', props);
   return (
     <div>
       Coffee store{router.query.id}
       <Link href="/">Back to home</Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
