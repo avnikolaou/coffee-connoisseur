ee97ae1fed3916f99eb2b5936b38cd258b199749
avnik <avnikolaou@hotmail.com>
2022-06-21 11:36:19 +0300

    feat: foursquare api to get coffee stores

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,14 @@ import isEmpty from 'lodash/isEmpty';
 import Banner from '../components/banner';
 import Card from '../components/card';
 
+import { fetchCoffeeStores } from '../lib/coffee-stores';
+
 import styles from '../styles/Home.module.css';
 
-import coffeeStoresData from '../data/coffee-stores.json';
-
 export async function getStaticProps(context) {
+  const coffeeStores = await fetchCoffeeStores();
   return {
-    props: { coffeeStores: coffeeStoresData }, // will be passed to the page component as props
+    props: { coffeeStores }, // will be passed to the page component as props
   };
 }
 
@@ -47,11 +48,14 @@ const Home = ({ coffeeStores }) => {
               {coffeeStores.map((c) => {
                 return (
                   <Card
-                    key={c.id}
+                    key={c.fsq_id}
                     className={styles.card}
                     name={c.name}
-                    imgUrl={c.imgUrl}
-                    href={`/coffee-store/${c.id}`}
+                    imgUrl={
+                      c.imgUrl ||
+                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                    }
+                    href={`/coffee-store/${c.fsq_id}`}
                   />
                 );
               })}
