040bdbfb70b96592c3f46d2301a16f99b4219bad
Avraam Nikolaou <avnikolaou@outlook.com>
2022-07-08 14:36:20 +0300

    feat: createCoffeeStore api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,6 +1,32 @@
+import isEmpty from 'lodash/isEmpty';
+
 const Airtable = require('airtable');
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.BASEKEY
+  process.env.AIRTABLE_BASE_KEY
 );
 
 const table = base('coffee-stores');
+
+console.log('TABLE: ', table);
+
+const createCoffeeStore = async (req, res) => {
+  if (req.method === 'POST') {
+    const findCoffeeStoreRecords = await table
+      .select({
+        filterByFormula: `id="0"`,
+      })
+      .firstPage();
+
+    console.log('CoffeeStore: ', findCoffeeStoreRecords);
+
+    if (!isEmpty(findCoffeeStoreRecords)) {
+      res.json(findCoffeeStoreRecords);
+    } else {
+      res.json({ message: 'create a record' });
+    }
+  } else {
+    res.json({ message: 'GETTING!' });
+  }
+};
+
+export default createCoffeeStore;
