6fc42148a64ee787b18ed525c3e6c9c532897f74
Avraa <avnikolaou@outlook.com>
2022-07-20 10:24:08 +0300

    quality: add 'alt' for images in coffee-store

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -174,17 +174,32 @@ const CoffeeStore = (initialProps) => {
 
         <div className={cls('glass', styles.col2)}>
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/places.svg" width={24} height={24} />
+            <Image
+              src="/static/icons/places.svg"
+              width={24}
+              height={24}
+              alt="Icon"
+            />
             <p className={styles.text}>{address}</p>
           </div>
 
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/nearMe.svg" width={24} height={24} />
+            <Image
+              src="/static/icons/nearMe.svg"
+              width={24}
+              height={24}
+              alt="Icon"
+            />
             <p className={styles.text}>{locality || neighbourhood}</p>
           </div>
 
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/star.svg" width={24} height={24} />
+            <Image
+              src="/static/icons/star.svg"
+              width={24}
+              height={24}
+              alt="Icon"
+            />
             <p className={styles.text}>{votingCount}</p>
           </div>
 
